6b54af6ece1bd0600cd0da30491c80ad3b806519
Josh Ross <josh.ross1234@gmail.com>
2015-09-07 18:03:10 -0400

    Create ClassWorki.java

---
+++ ClassWorki.java
@@ -0,0 +1,13 @@
+/*
+ * Classwork from 08_31_15
+ */
+
+public class ClassWorki {
+    public static void main(String[] args) {
+        int i = 3; //defining a variable
+        if (i == 3) {
+            i++;
+            System.out.println("hi");
+        }
+    }
+}
